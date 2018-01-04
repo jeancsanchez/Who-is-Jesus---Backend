6a6f3d608d3933e975bc960f49667b1ebdad4879
jeancsanchez <jeancs.patricio@gmail.com>
2018-01-04 20:56:34 -0300

    Update verse model

--- verses/models.py
+++ verses/models.py
@@ -3,9 +3,29 @@ from __future__ import unicode_literals
 from django.db import models
 
 
+# noinspection SpellCheckingInspection
 class Verse(models.Model):
-    verse = models.CharField(max_length=300, null=True)
+    GOSPELS = (
+        ('Mt', 'Mateus'),
+        ('Lc', 'Lucas'),
+        ('Mc', 'Marcos'),
+        ('Jo', 'João'),
+        ('At', 'Atos dos apóstolos'),
+        ('Rom', 'Romanos'),
+        ('I Cor', 'Primeira epístola Coríntios'),
+        ('II Cor', 'Segunda epístolas Coríntios'),
+        ('Gal', 'Gálatas'),
+        ('Ef', 'Efésios'),
+        ('Flp', 'Filipenses'),
+        ('Col', 'Colossenses'),
+        ('I Tes', 'Primeira epístola Tessalonicenses'),
+        ('II Tes', 'Segunda epístola Tessalonicenses'),
+        ('I Tim', 'Primeira epístola a Timóteo'),
+        ('II Tim', 'Primeira epístola a Timóteo')
+    )
+
+    verse = models.TextField(max_length=300, null=True)
+    gospel = models.CharField(max_length=8, choices=GOSPELS)
     chapter_number = models.IntegerField(default=0, null=True)
     verse_begin = models.IntegerField(default=1, null=True)
     verse_end = models.IntegerField(default=1, null=True)
-

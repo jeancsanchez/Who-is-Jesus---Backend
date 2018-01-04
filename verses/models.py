1f56ee3c244f946fae2abae8b304f0b5f7a9aacd
jeancsanchez <jeancs.patricio@gmail.com>
2018-01-04 20:25:45 -0300

    Add the first model

--- verses/models.py
+++ verses/models.py
@@ -2,4 +2,10 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Verse(models.Model):
+    verse = models.CharField(max_length=300, null=True)
+    chapter_number = models.IntegerField(default=0, null=True)
+    verse_begin = models.IntegerField(default=1, null=True)
+    verse_end = models.IntegerField(default=1, null=True)
+

40f9aae6376b756a2c3e24de683974c38b00017f
jeancsanchez <jeancs.patricio@gmail.com>
2018-01-04 23:40:07 -0300

    Filtering verse by magic number.

--- verses/views.py
+++ verses/views.py
@@ -1,6 +1,8 @@
+from builtins import len, int
 from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
+from rest_framework.decorators import list_route
 from rest_framework.response import Response
 from rest_framework.viewsets import ViewSet
 
@@ -22,4 +24,19 @@ class VerseViewSet(ViewSet):
         queryset = Verse.objects.all()
         verse = get_object_or_404(queryset, pk=pk)
         serializer = VerseSerializer(verse)
+        return Response(serializer.data)
+
+    @list_route(methods=['GET'])
+    def filter(self, request):
+        gospel_key = request.query_params.get('gospel_key')
+        magic_number = int(request.query_params.get('magic_number'))
+
+        queryset = Verse.objects.filter(gospel=gospel_key)
+        len_queryset = len(queryset)
+        index = int(magic_number/len_queryset)
+
+        if index > len_queryset:
+            index = int(index/len_queryset)
+
+        serializer = VerseSerializer(queryset[index])
         return Response(serializer.data)
